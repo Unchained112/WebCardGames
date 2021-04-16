825fef0e4ccd9aa6fa3d528ec553d70049c18a99
bosen <chengbosen112@gmail.com>
2021-04-16 20:49:00 +0800

    Update breakoutPresenter.js

--- public/vuejs/breakoutPresenter.js
+++ public/vuejs/breakoutPresenter.js
@@ -2,16 +2,7 @@ const BreakoutPresenter = {
     props:["model"],
     data(){ return {promise:null, data:null, error:null};},
 	created(){ 
-        if(BREAKOUT_DECK_ID_FLAG){
-            this.promise = CardSource.drawCards(DECK_ID_BREAKOUT_1, 52);
-            BREAKOUT_DECK_ID_FLAG = false;
-            CardSource.reShuffle(DECK_ID_BREAKOUT_2);
-        }
-        else{
-            this.promise = CardSource.drawCards(DECK_ID_BREAKOUT_2, 52);
-            BREAKOUT_DECK_ID_FLAG = true;
-            CardSource.reShuffle(DECK_ID_BREAKOUT_1);
-        }
+        this.promise = Promise.all([CardSource.drawCards(DECK_ID_BREAKOUT_1, 52), CardSource.reShuffle(DECK_ID_BREAKOUT_1)]);
     },          // lifecycle 1, execute at creation
 	watch:{
         'promise': {   // note: not this.promise! 
@@ -20,12 +11,12 @@ const BreakoutPresenter = {
 		        this.data = this.error = null;
                 if(this.promise){
 			        const p = this.promise;
-                    this.promise.then(dt=>{if(this.promise===p){this.data = dt;console.log(this.data)} }).catch(er=>{if(this.promise===p){}});
+                    this.promise.then(dt=>{if(this.promise===p){this.data = dt[0];console.log(this.data)} }).catch(er=>{if(this.promise===p){}});
                 }
             }
         }
     },
-    render(){      
+    render(){
         return <div>
             {promiseNoData(this.promise, this.data, this.error) || <BreakoutView breakoutModel={this.model} breakout_data={this.data}/>}
         </div>
