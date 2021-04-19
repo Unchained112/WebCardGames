9d1c4dbf07b58168ba944fdc0cf350f0e7a7fa63
Yating Liu <stephanie_ytt@hotmail.com>
2021-04-19 21:26:54 +0800

    24Game change on start page

--- public/js/views/startViews.js
+++ public/js/views/startViews.js
@@ -1,9 +1,37 @@
 function StartView(props){
     return(
-        <div class="w3-container w3-center">
-            <h2>24 Game</h2>
-            <p></p>
-            <button class="w3-button w3-green" onClick={e=>{window.location.hash="#24gameContent"}}>Start!</button>
+        <div className="game24start">
+            <div className="w3-container w3-center">
+                <div className="w3-container w3-margin-top">
+                    <img src={"./assets/24gamelogowhite.png"} style="width:200px"></img>
+                </div>
+                <div class="w3-content w3-text-white" style="max-width:500px">
+                    <p>The 24 game is a simple math game played with poker cards.The player draws four cards from a shuffled deck of cards and find a way to manipulate the four
+                        values with elementary arithmetic operations(+,-,*,/) to come up with 24. </p>
+                </div>
+
+                <button className="w3-button w3-green" onClick={e => {
+                    window.location.hash = "#24gameContent"
+                }}>Start!
+                </button>
+                <div class="w3-text-white">
+                    <p>A simple example:</p>
+                    <p><img src={"./assets/24gameJD.png"} style="width:100px"></img>
+                        <img src={"./assets/24game7S.png"} style="width:100px"></img>
+                        <img src={"./assets/24gameAC.png"} style="width:100px"></img>
+                        <img src={"./assets/24game6H.png"} style="width:100px"></img>
+                    </p>
+                    <p>
+                        {"The quadruple is {11,7,1,6}. There are several different solutions:"}
+                    </p>
+                    <p> (11 - 7) * (1 * 6)</p>
+                    <p> (11 - 7) / (1 / 6)</p>
+                    <p> (11 + 7) + (1 * 6)</p>
+                    <p>...</p>
+
+                </div>
+            </div>
         </div>
+
     )
 }
