4e50c0ecc1576f695a474b06b41f2b6ca478ebd0
Yating Liu <stephanie_ytt@hotmail.com>
2021-04-25 13:34:13 +0800

    make loading icon centered

--- public/js/views/promiseNoData.js
+++ public/js/views/promiseNoData.js
@@ -3,7 +3,11 @@ function promiseNoData(promise, data, error){
         return (<span> no data </span>)
     }
 	if (promise != null && data === null && error === null){
-        return (<img src="assets/loading2.gif" style="width:100px"/>)
+        return (
+            <div class="w3-container w3-center">
+                <img src="assets/loading2.gif" style="width:100px"/>
+            </div>
+            )
     }
 	if (promise != null && data === null && error != null){
         return (<span style="color:red" > {error} </span>)
