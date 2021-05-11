f986f691098754ecc5dc6060f0705ad568231768
yifeiw1218 <79217239+yifeiw1218@users.noreply.github.com>
2021-05-11 17:27:50 +0800

    bj basic finished

--- public/js/views/blackjackViews.js
+++ public/js/views/blackjackViews.js
@@ -1,8 +1,32 @@
 function BlackjackView(props){
     const player=props.model.player;
     const host=props.model.host;
-    console.log(player);
-    console.log(host);
+    //console.log(player);
+    //console.log(host);
+
+    var checkwin = function(){
+        if(props.model.playersum>21){
+            return <div class="w3-center w3-text-white"> You Lose! Press "Play Again"</div>
+        }
+        if(props.model.playersum===21){
+            return <div class="w3-center w3-text-white"> You Win!!!!</div>
+        }
+        else{
+            return <p></p>
+        }
+    }
+    var checkwin_host = function(){
+        if(props.model.hostsum>21){
+            return <div class="w3-center w3-text-white"> You Win!!!!</div>
+        }
+        if(props.model.hostsum>props.model.playersum && props.model.hostsum<=21){
+            return <div class="w3-center w3-text-white"> You Lose! Press "Play Again"</div>
+        }
+        else{
+            return <p></p>
+        }
+    }
+
     return(
         <div id="blackjack" >
             <div id="blackjack" class="w3-container">
@@ -18,6 +42,12 @@ function BlackjackView(props){
                 )
             }
             </div>
+            
+            <p> </p>
+            <p> </p>
+            {checkwin()}{checkwin_host()}
+            <p> </p>
+            <p> </p>
 
             <div class="w3-container">
                 <button class="w3-button w3-green w3-round-xlarge" onClick={props.start}>Start </button> 
