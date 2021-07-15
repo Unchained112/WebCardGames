598d183020ba6b9456ecd3165f2967d39efd7a98
bosen <chengbosen112@gmail.com>
2021-07-15 20:50:04 +0800

    Update api

--- public/js/apiConfig.js
+++ public/js/apiConfig.js
@@ -1,12 +1,12 @@
 const BASE_URL="https://deckofcardsapi.com/api/deck/"; // Deck of Cards: an API
 const BASE_URL_GAME_24_EXAM="https://helloacm.com/api/24/";
-const DECK_ID_BREAKOUT_1 = "rq4v0sbh889t";
-const DECK_ID_BREAKOUT_2 = "zefpqhp4csun";
-const DECK_ID_24GAME_1= "qf4rvii61dzo";
-const DECK_ID_24GAME_2= "29z5onkzsqiy";
-const DECK_ID_TEXAS_1= "3p40paa87x90";
-const DECK_ID_TEXAS_2= "3p40paa87x90";
-const DECK_ID_BLACKJACK="l19kmbzinudk";
+const DECK_ID_BREAKOUT_1 = "26yxcwhp5hxt";
+const DECK_ID_BREAKOUT_2 = "nzkjnduzq8md";
+const DECK_ID_24GAME_1= "xvbjc57jigq7";
+const DECK_ID_24GAME_2= "diecf2pbh6dv";
+const DECK_ID_TEXAS_1= "4p2g3amowr54";
+const DECK_ID_TEXAS_2= "adno233lxvp4";
+const DECK_ID_BLACKJACK="9ex2npuer084";
 
 var LastGameState; //Store information about last states in Freecell
 var SingleState; //Store one last step state
